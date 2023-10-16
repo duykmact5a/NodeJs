678de33338b70a167ec71defc0d74a511e10f589
duykmact5a <146912154+duykmact5a@users.noreply.github.com>
2023-10-16 13:42:32 +0700

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,23 @@
+
+let app = express();
+
+app.engine('handlebars', handlebar({defaultlayout:'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static('pulic'));
+
+app.get('/', (req, res)=>{
+    res.sendFileS(path.realove(__dirname + '/public/index.html'));
+});
+app.get('/gift', (req,res)=>{
+    res.send("9GB lonk :http://**************************");
+});
+app.get('/about', (req,res)=>{
+    res.send("This is about");
+});
+app.listen(6969, (err)=>{
+    if(err) {console.log(err);}
+    console.log("App is start at port 6969");
+});
